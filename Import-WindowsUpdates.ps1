09e11091fe5bf6023c02f9e0cfd83a733a375afe
Dylan Tyler <47014517+Dylan-Tyler@users.noreply.github.com>
2023-04-06 10:20:45 -0500

    Add files via upload

---
+++ Import-WindowsUpdates.ps1
@@ -0,0 +1,10 @@
+﻿#Variables
+$TaskName = "Windows Updates - Weekly"
+$user = "System" 
+
+#create a scheduled task with powershell
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "C:\IT\Software\PS2EXE-GUI\POSH\WindowsUpdatesv3.exe"
+$Trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Tuesday -At 9am
+$ScheduledTask = New-ScheduledTask -Action $action -Trigger $trigger  
+
+Register-ScheduledTask -TaskName $TaskName -InputObject $ScheduledTask -User $user
