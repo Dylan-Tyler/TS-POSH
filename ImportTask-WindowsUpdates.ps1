41be97aa25c41bc3c1e5d19f3dd88145c95aa86b
Dylan Tyler <47014517+Dylan-Tyler@users.noreply.github.com>
2023-04-06 11:36:13 -0500

    Update ImportTask-WindowsUpdates.ps1

--- ImportTask-WindowsUpdates.ps1
+++ ImportTask-WindowsUpdates.ps1
@@ -3,7 +3,7 @@ $TaskName = "Windows Updates - Weekly"
 $user = "System" 
 
 #create a scheduled task with powershell
-$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "C:\IT\Software\PS2EXE-GUI\POSH\WindowsUpdatesv3.exe"
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "C:\TS\WindowsUpdates\settings.xml"
 $Trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Tuesday -At 9am
 $ScheduledTask = New-ScheduledTask -Action $action -Trigger $trigger  
 
